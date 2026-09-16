ae15fea320a9ffc832584caf7dd11ff8a05ab3b2
Ismail Mwarumba Mashine <mwarumba254@outlook.com>
2026-09-16 11:39:54 +0300

    Fix GitHub Pages CV routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import "./index.css";
-import App from "./App.tsx";
-import CVPage from "./CVPage.tsx";
+import App from "./App";
+import CVPage from "./CVPage";
+
+const redirect = sessionStorage.getItem("redirect");
+
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+
+  if (
+    redirect !== "/" &&
+    redirect !== window.location.pathname + window.location.search + window.location.hash
+  ) {
+    window.history.replaceState(null, "", redirect);
+  }
+}
 
 const path = window.location.pathname.replace(/\/+$/, "") || "/";
 
